a68f1da69f87200cef59ccb2dce22b3c4e7982a2
Danyer <danjer0826@gmail.com>
2019-09-01 23:56:14 -0400

    Solved problem 2

--- KotlinFoundation/src/Program.kt
+++ KotlinFoundation/src/Program.kt
@@ -6,6 +6,6 @@ fun main(args: Array<String>) {
     println("Choose an euler problem")
     when (readLine()!!.toInt()) {
         1 -> euler.problem1()
-        2 -> euler.problem2("Hello")
+        2 -> euler.problem2()
     }
 }
--- KotlinFoundation/src/models/Euler.kt
+++ KotlinFoundation/src/models/Euler.kt
@@ -13,5 +13,20 @@ class Euler() {
         println("Sum of numbers multiplies of 3 and 5 is: $counter")
     }
 
-        var problem2 = {sample: String -> println(sample)}
+    fun problem2() {
+        var counter = 0
+        var currentNumber = 1
+        var previousNumber = 1
+
+        while (currentNumber < 4000000) {
+         if (currentNumber % 2 == 0) {
+             counter += currentNumber
+         }
+            currentNumber += previousNumber
+            previousNumber = currentNumber - previousNumber
+
+        }
+
+        println("The sum of fibonacci sequence under 400000 is: $counter")
+    }
 }
